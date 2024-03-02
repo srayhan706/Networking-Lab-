4dae47afbf352ae3ae1232aa9e2797246c40b92a
srayhan706 <srayhan706@gmail.com>
2024-03-02 20:55:07 +0600

    maybe task2 of lab5 done

--- lab5/Task 2/client.py
+++ lab5/Task 2/client.py
@@ -1,7 +1,7 @@
 import socket
 import time
+import random
 
-# Function to create TCP segment header
 def makeheader(seqNum=0, ackNum=0, ack=0, sf=0, rwnd=0):
     header = seqNum.to_bytes(4, byteorder="little")
     header += ackNum.to_bytes(4, byteorder="little")
@@ -10,7 +10,6 @@ def makeheader(seqNum=0, ackNum=0, ack=0, sf=0, rwnd=0):
     header += rwnd.to_bytes(2, byteorder="little")
     return header
 
-# Function to parse TCP segment header
 def fromheader(segment):
     seqNum = int.from_bytes(segment[:4], byteorder="little")
     ackNum = int.from_bytes(segment[4:8], byteorder="little")
@@ -19,97 +18,70 @@ def fromheader(segment):
     rwnd = int.from_bytes(segment[10:12], byteorder="little")
     return seqNum, ackNum, ack, sf, rwnd
 
-# Function to calculate SampleRTT using EWMA
-def calculate_sample_rtt(sample_rtt, alpha, new_rtt):
-    return alpha * sample_rtt + (1 - alpha) * new_rtt
+def calculate_timeout(sample_rtt, alpha=0.125):
+    return (1 - alpha) * timeout + alpha * sample_rtt
 
-# Function to calculate Timeout using EWMA
-def calculate_timeout(sample_rtt, estimated_rtt, alpha):
-    return alpha * estimated_rtt + (1 - alpha) * sample_rtt
+cl_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serv_addr = ('127.0.0.1', 8888)
+cl_socket.connect(serv_addr)
 
-# Function to send data packets and handle acknowledgments
-def send_data_packets(cl_socket, data, data_len, mss, rec_buf_size, alpha):
-    head_len = 12
-    win_size = mss
-    seq_num = 0
-    exp_ack_num = 0
-    rec_win = 50
-    sent_size = 0
-    dup_ack = 0
-    last_ack = 0
-    sample_rtt = 0
-    estimated_rtt = 0
-    timeout = 1  # Initial timeout value
-    start_time = time.time()
+head_len = 12
+rec_buf_size = 4
+mss = 20
+win_size = 20
+cl_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, rec_buf_size)
 
-    # Open files to store SampleRTT and timeout
-    sample_rtt_file = open("sample_rtt.txt", "w")
-    timeout_file = open("timeout.txt", "w")
+seq_num = 0
+exp_acq_num = 0
 
+filename = "send.txt"
+with open(filename, "rb") as file:
+    data = file.read()
+data_len = len(data)
+print(f"Length of the file {data_len}")
+
+rec_win = 50
+sent_size = 0
+dup_ack = 0
+last_ack = 0
+sample_rtt = []
+timeout = 1
+
+with open("sample_rtt.txt", "w") as sample_rtt_file, open("timeout.txt", "w") as timeout_file:
     while seq_num < data_len:
+        start_time = time.time()
         curr_sent_size = 0
         while curr_sent_size < win_size and seq_num < data_len:
             send_size = min(mss, data_len - seq_num)
-            send_time = time.time()  # Start timer
-            cl_socket.sendall(makeheader(seq_num, seq_num, 0, 0, 0) + data[seq_num:seq_num + send_size])
-            print(f"Data sent with sequence number {seq_num}")
+            cl_socket.sendall(makeheader(seq_num, seq_num, 0, 0, rec_win) + data[seq_num:seq_num + send_size])
+            print(f"Data sent of sequence number {seq_num}")
             curr_sent_size += send_size
             sent_size += send_size
             seq_num += send_size
 
-            # Receive ACK and calculate SampleRTT
-            ack_pkt = cl_socket.recv(head_len)
-            recv_time = time.time()  # Stop timer
-            sample_rtt = recv_time - send_time
-            estimated_rtt = calculate_sample_rtt(sample_rtt, alpha, estimated_rtt)
-            timeout = calculate_timeout(sample_rtt, estimated_rtt, alpha)
+        exp_acq_num = seq_num
+        ack_pkt = cl_socket.recv(head_len)
+        print("Acknowledgment packet received")
+        seqNum, ack_num, ack, sf, rec_win = fromheader(ack_pkt)
+        print(f"Sequence number {seqNum}, expected Ackn_Number {exp_acq_num}, Ackn_Number {ack_num}")
+        sample_rtt_value = time.time() - start_time
+        sample_rtt.append(sample_rtt_value)
+        sample_rtt_file.write(str(sample_rtt_value) + "\n")
 
-            # Write SampleRTT and timeout to files
-            sample_rtt_file.write(f"{sample_rtt}\n")
-            timeout_file.write(f"{timeout}\n")
+        timeout = calculate_timeout(sample_rtt_value)
+        timeout_file.write(str(timeout) + "\n")
 
-            print("Acknowledgement packet received")
-            seqNum, ack_num, ack, sf, rec_win = fromheader(ack_pkt)
-            print(f"Sequence number {seqNum}, expected Ackn_Number {exp_ack_num}, Ackn_Number {ack_num}")
-            win_size = min(2 * rec_buf_size, rec_win)
+        win_size = min(2 * rec_buf_size, rec_win)
 
-            # Implement Cumulative ACK
-            if ack_num == last_ack:
-                dup_ack += 1
-            else:
-                dup_ack = 0
-            if dup_ack == 3:
-                print("Received Triple Duplicate Acknowledgement, go back to last_ack")
-                dup_ack = 0
-                seq_num = last_ack
+        if ack_num == last_ack:
+            dup_ack += 1
+        else:
+            dup_ack = 0
+        if dup_ack == 3:
+            print("Received Triple Duplicate Acknowledgment, go back to last_ack")
+            dup_ack = 0
+            seq_num = last_ack
 
-            last_ack = ack_num
+        last_ack = ack_num
 
-    # Close files
-    sample_rtt_file.close()
-    timeout_file.close()
-
-    print("Data transmission completed.")
-
-# Main function to initiate the client
-def main():
-    cl_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serv_addr = ('127.0.0.1', 8888)
-    cl_socket.connect(serv_addr)
-
-    rec_buf_size = 4
-    mss = 20
-    alpha = 0.125  # Alpha value for EWMA
-
-    filename = "send.txt"
-    with open(filename, "rb") as file:
-        data = file.read()
-    data_len = len(data)
-    print(f"Length of the file: {data_len}")
-
-    send_data_packets(cl_socket, data, data_len, mss, rec_buf_size, alpha)
-
-    cl_socket.close()
-
-if __name__ == "__main__":
-    main()
+cl_socket.close()
